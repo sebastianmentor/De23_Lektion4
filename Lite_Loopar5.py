59e55185af786d4425f8d876d68a2627e549d2b9
Sebastianmentor <sebastian.ohman@systementor.se>
2023-09-07 15:46:17 +0200

    Lite exempel om if också!

--- Lite_Loopar5.py
+++ Lite_Loopar5.py
@@ -8,13 +8,22 @@
 
 # Med lista
 
-lista_med_tal = []
+# lista_med_tal = []
 
-for _ in range(10):
-    lista_med_tal.append(int(input('Skriv in tal: ')))
+# for _ in range(10):
+#     lista_med_tal.append(int(input('Skriv in tal: ')))
 
-print('Summan av talen:', end="")
-for i in lista_med_tal:
-    print(i, end=' ')
+# print('Summan av talen:', end="")
+# for i in lista_med_tal:
+#     print(i, end=' ')
 
-print('=', sum(lista_med_tal))
+# print('=', sum(lista_med_tal))
+
+pengar = int(input('Skriv hur mycket pengar du har!'))
+har_rabbat = input('Har du rabbat? (j/n)')
+
+if (15<= pengar <= 25) and har_rabbat == 'n':
+    print('liten burgare')
+elif 15<= pengar and pengar <= 25 and har_rabbat == 'j':
+    print('Du kan köpa en liten hamburgare och en pommes frites')
+...
