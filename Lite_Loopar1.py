e3383b8bc788e8822a9eb13deae24fa6697d8dcb
Sebastianmentor <sebastian.ohman@systementor.se>
2023-09-07 11:00:45 +0200

    first commit

---
+++ Lite_Loopar1.py
@@ -0,0 +1,41 @@
+# Lite loopar
+
+#Ett program som körs tills användaren gissat meningen med livet
+# guess = input('Gissa vad meningen med livet är?')
+
+# while guess != '42':
+#     print('Ha!!!! Du gissa fel!')
+#     guess = input('Igen! Gissa vad meningen med livet är?')
+
+# print('Gratulerar!! Du gissa rätt!')
+
+#----------------------------------------------------------
+# while(True):
+#     gissa = input("Gissa meningen med livet!: ")
+#     if gissa == "42":
+#         print("Grattis du gissade rätt!")
+#         break
+#     else:
+#         print("HAAHAHA, Du gissade fel! ")
+#----------------------------------------------------------
+less_then_hundred = 0
+
+while True:
+    print(less_then_hundred)
+    less_then_hundred += 1
+    if less_then_hundred >= 99:
+        break
+
+#----------------------------------------------------------
+# less_then_five = 0
+# run_program = True
+# while run_program:
+
+#     print(less_then_five)
+
+#     less_then_five += 1
+
+#     if less_then_five >= 4:
+#         run_program = False
+
+# print(less_then_five)
---
+++ Lite_Loopar2.py
@@ -0,0 +1,24 @@
+# Lite for-loop
+
+# name = "Sebastian"
+
+# for bokstav in name:
+#     print(bokstav, end=' <--')
+
+#-------------------------------------------------
+
+# name = 'Kristofer'
+
+# for index in range(len(name)):
+#     print(name[index], end='<---')
+
+#-------------------------------------------------
+# import os 
+# import time
+# for j in range(10):
+#     os.system('cls') # Mac+ Linux använd 'clear'
+#     time.sleep(0.5)
+
+#     for i in range(1,15,2):
+#         print((i*'*').center(18))
+#     time.sleep(0.5)
